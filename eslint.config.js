a01da6d7bb0c8933ccc18f4c4a0be1e611eedcb9
Bedmor <besim.ozdemir@ogr.sakarya.edu.tr>
2025-11-15 01:40:31 +0300

    yaw

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,7 @@ export default tseslint.config(
     ],
     rules: {
       "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-redundant-type-constituents": "off",
       "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/prefer-nullish-coalescing": "off",
