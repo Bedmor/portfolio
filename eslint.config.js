58e51b7cf269295f39802d2cafd533ce10c8bccf
Bedmor <besim.ozdemir@ogr.sakarya.edu.tr>
2025-11-15 01:38:05 +0300

    welp

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,10 @@ export default tseslint.config(
       ...tseslint.configs.stylisticTypeChecked,
     ],
     rules: {
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/prefer-nullish-coalescing": "off",
+      "no-img-element": "off",
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/consistent-type-imports": [
